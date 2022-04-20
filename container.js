271e19a9ff4c1a7c53d0228cabffc50b1c77656b
Pablo <delatorre.pablodaniel@gmail.com>
2022-04-20 16:37:56 -0300

    terminado sin funcionar

--- container.js
+++ container.js
@@ -6,25 +6,105 @@ class Container {
         this.numId = 0
         this.lista = []
     }
-    async init(){
+    
+    async save(objeto) {
         try {
-            const data = await fs.promises.readFile(this.arch)
-            this.lista = JSON.parse(data)
-            this.lista.forEach(i => {
-                if(i.id > this.numId) this.numId = i.id
+            let informacion = await fs.promises.readFile(this.arch, "utf-8")
+            let informacionUtil = JSON.parse(informacion)
+            console.log(informacion)
+            console.log(informacionUtil)
+            if (!informacionUtil.includes(objeto.id)){
+                informacionUtil.push(objeto)
+                console.log(informacionUtil)
+                let informacionStr = JSON.stringify(informacionUtil)
+                let write = await fs.promises.writeFile(this.arch, informacionStr)
+                return write
+            }
+        } catch (error) {
+            console.log('error',error)
+        }
+    }
+
+    async getById(numero){
+        try {
+            let informacion = await fs.promises.readFile(this.arch, "utf-8")
+            let informacionUtil = JSON.parse(informacion)
+            informacionUtil.forEach(x => {
+                if (x.id == numero){
+                    console.log(x)
+                    return x
+                } else {
+                    return null
+                }
             })
+        } catch (error) {
+            console.log('error',error)
         }
-        catch (error){
-            console.log("Archivo no encontrado")
-        }    
     }
-    async write(){
+
+    async getAll() {
         try {
-            const string = JSON.stringify(this.lista)
-            await fs.promises.writeFile(this.arch, string)
-        }
-        catch(err) {
-            console.log(err)
+            let informacion = await fs.promises.readFile(this.arch, "utf-8")
+            let informacionUtil = JSON.parse(informacion)
+            console.log(informacionUtil)
+            return informacionUtil
+        } catch (error) {
+            console.log('error',error)
         }
     }
+
+    async deleteById(numero){
+        try {
+            let informacion = await fs.promises.readFile(this.arch, "utf-8")
+            let informacionUtil = JSON.parse(informacion)
+            informacionUtil.forEach(x => {
+                if (x.id == numero){
+                    let indice = indexOf(x)
+                    let informacionStr =JSON.stringify(informacionUtil.splice(indice))
+                    let write = fs.promises.writeFile(this.arch, informacionStr)
+                    return write
+                } else {
+                    return null
+                }
+            })
+        } catch (error) {
+            console.log('error',error)
+        }
+    }
+
+    async deletAll() {
+        try {
+            fs.promises.unlinkSync(this.arch)
+            //file removed
+            } catch(err) {
+                console.error(err)
+            }
+    }
+
+
+    // async init(){
+    //     try {
+    //         const data = await fs.promises.readFile(this.arch,"utf-8")
+    //         this.lista = JSON.parse(data)
+    //         this.lista.forEach(i => {
+    //             if(i.id > this.numId) this.numId = i.id
+    //         })
+    //     }
+    //     catch (error){
+    //         console.log("Archivo no encontrado")
+    //     }    
+    // }
+    // async write(){
+    //     try {
+    //         const string = JSON.stringify(this.lista)
+    //         await fs.promises.writeFile(this.arch, string)
+    //     }
+    //     catch(err) {
+    //         console.log(err)
+    //     }
+    // }
+
 }
+
+// let contenedor = new Container('./productos.txt')
+// contenedor.save()
---
+++ test.js
@@ -0,0 +1,31 @@
+const Container = require('./container')
+const obj = {title:"Escuadra", price:123, id:1}
+
+const play = async function(){
+    
+    
+    let contenedor = new Container('./productos.txt')
+    contenedor.save(obj)
+    contenedor.getById(1)
+    contenedor.getAll()
+    // contenedor.deleteById(3)
+    // contenedor.deleteAll()
+    
+    
+    // const arch = new Container(productos.json)
+    // await arch.init()
+
+    // const save = await arch.save({})
+    // console.log(`Item con id ${save}`)
+
+    // const unId = arch.getById(1)
+
+    // const todos = arch.getAll()
+    // console.log(`${todos}`)
+
+    //const borrarUno = arch.deleteById(2)
+
+    //const borrarTodos = arch.deleteAll()
+} 
+
+play()
